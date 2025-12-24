108a13787c839089297790fd6a4b910bdb765e53
Adam <adammertz@gmail.com>
2025-12-24 16:30:37 -0600

    Remove unused custom flag parsing logic from `config.go`

--- hit/cmd/hit/config.go
+++ hit/cmd/hit/config.go
@@ -2,25 +2,7 @@ package main
 
 import (
 	"flag"
-	"strconv"
 )
-
-type parseFunc func(string) error
-
-func stringVar(p *string) parseFunc {
-	return func(s string) error {
-		*p = s
-		return nil
-	}
-}
-
-func intVar(p *int) parseFunc {
-	return func(s string) error {
-		var err error
-		*p, err = strconv.Atoi(s)
-		return err
-	}
-}
 
 type config struct {
 	url string
