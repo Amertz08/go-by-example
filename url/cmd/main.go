11828e90606c855a966ee0701373250019128fd8
Adam <adammertz@gmail.com>
2025-12-23 23:21:24 -0600

    Enhance `main.go` with URL parsing error handling and detailed output display

--- url/cmd/main.go
+++ url/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/url"
+import (
+	"fmt"
+	"log"
+
+	"github.com/Amertz08/go-by-example/url"
+)
 
 func main() {
 	uri, err := url.Parse("https://github.com/inancgumus")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Scheme:", uri.Scheme)
+	fmt.Println("Host  :", uri.Host)
+	fmt.Println("Path  :", uri.Path)
+	fmt.Println(uri)
 }
