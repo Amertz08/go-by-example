067a228e3b26d5cfd50df3a1c6aa650a61da3117
Adam <adammertz@gmail.com>
2025-12-23 23:23:03 -0600

    Add comments to `URL` struct and functions in `url.go` for clarity

--- url/url.go
+++ url/url.go
@@ -2,16 +2,19 @@ package url
 
 import "fmt"
 
+// A URL represents a parse URL
 type URL struct {
 	Scheme string
 	Host   string
 	Path   string
 }
 
+// Parse parses a URL string into a URL structure.
 func (u *URL) String() string {
 	return fmt.Sprintf("%s://%s/%s", u.Scheme, u.Host, u.Path)
 }
 
+// String reassamples the URL into a URL string.
 func Parse(uri string) (*URL, error) {
 
 	return &URL{
