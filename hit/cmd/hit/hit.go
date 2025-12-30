60e3d73ab1250fa5281b3be4affd51813072b24e
Adam <adammertz@gmail.com>
2025-12-30 00:23:16 -0600

    Refactor `main` to use `env` struct for improved I/O handling and error reporting in `hit` tool

--- hit/cmd/hit/hit.go
+++ hit/cmd/hit/hit.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -13,11 +14,36 @@ const logo = `
   \/_/\/_/   \/_/     \/_/`
 
 func main() {
-	c := config{
-		n: 100, c: 10,
+	e := &env{
+		stdout: os.Stdout,
+		stderr: os.Stderr,
+		args:   os.Args[1:],
 	}
-	if err := parseArgs(&c, os.Args[1:]); err != nil {
+	if err := run(e); err != nil {
 		os.Exit(1)
 	}
-	fmt.Printf("%s\n\nSending %d requests to %q (concurrency: %d)\n", logo, c.n, c.url, c.c)
+}
+
+type env struct {
+	stdout io.Writer
+	stderr io.Writer
+	args   []string
+	dryrun bool
+}
+
+func run(e *env) error {
+	c := config{n: 100, c: 10}
+
+	if err := parseArgs(&c, e.args[1:], e.stderr); err != nil {
+		return err
+	}
+	fmt.Fprintf(
+		e.stdout,
+		"%s\n\nSendidng %d requeests to %q (concurrency: %d)\n",
+		logo,
+		c.n,
+		c.url,
+		c.c,
+	)
+	return nil
 }
