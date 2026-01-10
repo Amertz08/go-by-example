e2817e862351527e611d7eff6e841bbf56f3e365
Adam <adammertz@gmail.com>
2026-01-10 10:38:07 -0600

    Add `Options` struct and `Defaults` function to configure request concurrency, rate limiting, and sender in `hit` package

---
+++ hit/option.go
@@ -0,0 +1,39 @@
+package hit
+
+import "net/http"
+
+// SendFunc is a type of function that sends an
+// [http.Request] and retuns a [Result].
+type SendFunc func(*http.Request) Result
+
+// Options defines the options for sending requests.
+// Uses default options for unset options.
+type Options struct {
+	// concurrency is the number of concurrent requests to send.
+	// Default: 1
+	Concurrency int
+
+	// RPS is the requests to send per second.
+	// Default: 0 (no rate limiting)
+	RPS int
+
+	// Send processes requests.
+	// Default Uses [Send].
+	Send SendFunc
+}
+
+func Defaults() Options {
+	return withDefaults(Options{})
+}
+
+func withDefaults(o Options) Options {
+	if o.Concurrency == 0 {
+		o.Concurrency = 1
+	}
+	if o.Send == nil {
+		o.Send = func(r *http.Request) Result {
+			return Send(http.DefaultClient, r)
+		}
+	}
+	return o
+}
