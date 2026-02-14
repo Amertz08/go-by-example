13003ea1c125920e41270ad4d63f53a3f9df63ec
Adam <adammertz@gmail.com>
2026-02-14 15:28:32 -0600

    Refactor: relocate `roundTripperFunc` to `hit_test.go` for improved test organization

--- hit/cmd/hit/hit_test.go
+++ hit/cmd/hit/hit_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"net/http"
 	"strings"
 	"testing"
 )
@@ -9,12 +8,6 @@ import (
 type testEnv struct {
 	stdout strings.Builder
 	stderr strings.Builder
-}
-
-type roundTripperFunc func(*http.Request) (*http.Response, error)
-
-func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
-	return f(r)
 }
 
 func testRun(args ...string) (*testEnv, error) {
