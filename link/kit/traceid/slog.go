db27e81d2ef1837f26b56426f1c31850dc37a2f5
Adam <adammertz@gmail.com>
2026-02-16 10:11:40 -0600

    Add `LogHandler` to `traceid` for injecting trace IDs into log records

---
+++ link/kit/traceid/slog.go
@@ -0,0 +1,33 @@
+package traceid
+
+import (
+	"context"
+	"log/slog"
+)
+
+// LogKey is the key used to store the trace ID in the log record.
+const LogKey = "trace_id"
+
+// LogHandler adds trace IDs form [context.Context] to [slog.Record].
+type LogHandler struct {
+	slog.Handler // here is an example of field embedding
+	LogKey       string
+}
+
+// NewLogHandler returns a new [LogHandler].
+func NewLogHandler(h slog.Handler) slog.Handler {
+	return &LogHandler{
+		Handler: h,
+		LogKey:  LogKey,
+	}
+}
+
+// Handle may add a [slog.Attr] to the [slog.Record].
+func (h *LogHandler) Handle(ctx context.Context, r slog.Record) error {
+	// If the trace ID is present in the context, add it to the log record.
+	if id, ok := FromContext(ctx); ok {
+		r = r.Clone()
+		r.AddAttrs(slog.String(h.LogKey, id))
+	}
+	return h.Handler.Handle(ctx, r)
+}
