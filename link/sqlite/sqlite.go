076e41b93ad8142245eac93fbd1dfa0638e8d0ee
Adam <adammertz@gmail.com>
2026-02-18 12:53:49 -0600

    Add `Dial` function to `sqlite` package for establishing SQLite connections with schema initialization

--- link/sqlite/sqlite.go
+++ link/sqlite/sqlite.go
@@ -1,3 +1,21 @@
 package sqlite
 
-import _ "modernc.org/sqlite"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "modernc.org/sqlite"
+)
+
+// Dial connects ot SQLite and applies the schema for convenience.
+func Dial(ctx context.Context, dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("opening sqlite database: %w", err)
+	}
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("pinging sqlite database: %w", err)
+	}
+	return db, nil
+}
